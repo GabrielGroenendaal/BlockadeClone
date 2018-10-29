3225b5dcab532efd0ce0e4ab2fb94794b94172a8
Gabriel Groenendaal <gfg243@nyu.edu>
2018-10-29 09:00:04 -0400

    Additional Update

--- Blockade Clone Project/Assets/Scripts/Game Controller.cs
+++ Blockade Clone Project/Assets/Scripts/Game Controller.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class Script : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+
+	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown("space"))
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadScene("Main Game");
+		}
 	}
 }
