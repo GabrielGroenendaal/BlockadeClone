a9a27354fa8c2bee80a915fc0cb3f528bd5f4dc8
Gabriel Groenendaal <gfg243@nyu.edu>
2018-10-29 10:18:45 -0400

    Final update before class: adds a bit of code to reset the game.

--- Blockade Clone Project/Assets/Scripts/GameController.cs
+++ Blockade Clone Project/Assets/Scripts/GameController.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Script : MonoBehaviour {
+public class GameController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-
 	
 	// Update is called once per frame
 	void Update () {
