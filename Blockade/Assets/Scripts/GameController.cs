791248da28662ea20eb3ba0090095a2072b32868
DESKTOP-GH9GNQ6\Gabriel G <gfg243@nyu.edu>
2018-11-02 18:16:36 -0400

    The updated build for my Blockade clone. The work I've done is pretty good so far, setting up the whole grid and starting to get into the nitty gritty of game logic.

---
+++ Blockade/Assets/Scripts/GameController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	
+	// This code holds the board. The board is first
+	public GameObject[] board_base = new GameObject[320];
+	public GameObject[,] board = new GameObject[20,16];
+	
+	
+	// This code keeps track of each players "score" or health. When they reach 0 health, they die
+	private int Player1Score;
+	private int Player2Score;
+	
+	
+	// This is where we will store the player objects
+	public PlayerControl player_one;
+	public PlayerControl player_two;
+
+	// This code is to reference the player art assets
+	public Sprite player1; public Sprite player1Body; public Sprite player1Angry; public 
+	Sprite player2; public Sprite player2Body; public Sprite player2Angry;
+	public Sprite p1Up; public Sprite p1Down; public Sprite p1Left; public Sprite p1Right;
+	public Sprite p2Up; public Sprite p2Down; public Sprite p2Left; public Sprite p2Right;
+	
+	
+	// Use this for initialization of the game. It sets the players scores and sets up the basic board that will be used
+	void Start ()
+	{
+		player_one.score  = 5;
+		player_two.score  = 5;
+		BuildBasicBoard();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// This code will populate the 20 x 16 array with the contents of the board_base array, forming the game grid
+	// This code will be called at the start of the game and whenever they need to reset the game
+	
+	void BuildBasicBoard()
+	{
+		// This code will store a counter for iterating through the array board_base
+		int count = 0;
+		
+		for (int i = 0; i < 16; i++)
+		{
+			for (int k = 0; k < 20; k++)
+			{
+				board[k, i] = board_base[count];
+			}
+		}
+		
+		// We then set the positions of the players
+		board[5, 11].GetComponent<Image>().sprite = player1;
+		board[11, 5].GetComponent<Image>().sprite = player2;
+		player_one.setPosition(5, 11);
+		player_two.setPosition(11, 5);
+		
+		// And pass them the board to do movement
+		player_one.grid = this;
+		player_two.grid = this;
+	}
+	
+	
+	
+}
---
+++ Blockade/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour {
+	
+	// This code is to reference the player art assets
+	public Sprite happy; public Sprite body; public Sprite angry;  
+	public Sprite up; public Sprite down; public Sprite left; public Sprite right;
+	
+	// This code is for other important values, such as the player's current score and the inputs
+	public int score;
+	public int Xpos;
+	public int Ypos;
+	private String input;
+	private String previousInput;
+	public Text countText;
+
+	public GameController grid;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void move()
+	{
+		
+	}
+	
+	// Simple code to set the position of the Player
+	public void setPosition(int x, int y)
+	{
+		Xpos = x;
+		Ypos = y;
+	}
+
+	void setCountText()
+	{
+		countText.text = score.ToString();
+	}
+	
+	
+}
